8f5aca1aa57f0767b3ea789df943917d1b5e3e02
Adolfo Gomes <adolfo.gomes@al.infnet.edu.br>
2025-09-07 22:43:22 -0300

    configs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/projeto_reviver/', // Nome do seu repositório
   plugins: [
     react(),
     tailwindcss()
